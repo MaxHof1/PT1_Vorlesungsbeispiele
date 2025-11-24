1eeacc548595e4ce3ed46c4a24e072baaf08d4d0
lenn1s <lennart.roehrborn@outlook.de>
2025-11-24 14:38:34 +0100

    Vorlesung: Input und Output (Kompakte Version)

--- writeintext.c
+++ writeintext.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
-
 int main (void){
     FILE *out = fopen("test.txt", "w");
     if(out == NULL) return 1;
     const float radius = 3, pi = 3.14595;
     fprintf(out,"Der Flächeninhalt beträgt %.2f", pi * (radius * radius));
     fclose(out);
-    return 0;
-}
+    return 0;}
